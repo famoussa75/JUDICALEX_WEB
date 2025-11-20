39a11ba6c7d2692f35363d28b283410d8e0fe46c
@cmoussa808 <cmoussa808@gmail.com>
2025-11-20 10:24:34 +0000

    correction analytics

--- analytics/views.py
+++ analytics/views.py
@@ -7,5 +7,6 @@ from analytics.models import DailyStats
 
 @csrf_exempt
 def stats_data(request):
-    data = DailyStats.objects.order_by('date').values('date', 'total_visits', 'unique_ips')
+    data = DailyStats.objects.order_by('-date')[:7].values('date', 'total_visits', 'unique_ips')
+    data = data[::-1]
     return JsonResponse(list(data), safe=False)
